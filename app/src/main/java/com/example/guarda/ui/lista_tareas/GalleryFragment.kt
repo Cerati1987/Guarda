0a88930aff4105df5a68ae640bc385bf972b19ba
Juan Felipe Orozco Cortés <orozjuanfelipe@gmail.com>
2024-04-29 20:37:47 -0500

    Primera version con diseño app

---
+++ app/src/main/java/com/example/guarda/ui/lista_tareas/GalleryFragment.kt
@@ -0,0 +1,14 @@
+package com.example.guarda.ui.lista_tareas
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class GalleryFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/example/guarda/ui/lista_tareas/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.guarda.ui.lista_tareas
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
